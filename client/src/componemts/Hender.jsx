e6917bf16027777cdaeccd9b3bf9038b2dbda062
darshit15-02 <darshit.jbs07@gmail.com>
2025-06-06 16:39:30 +0530

    - login page

--- client/src/componemts/Hender.jsx
+++ client/src/componemts/Hender.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React, { use, useContext } from 'react'
 import { assets } from '../assets/assets'
+import { AppContext } from '../context/AppContext'
 
 const Hender = () => {
+
+    const {userData} = useContext(AppContext)
+
     return (
         <div className='flex flex-col items-center mt-20 px-4 text-center text-gray-800'>
 
             <img src={assets.header_img} alt='header' className='w-36 h-36 rounded-full mb-6' />
 
-            <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey Develpoer
+            <h1 className='flex items-center gap-2 text-xl sm:text-3xl font-medium mb-2'>Hey {userData ? userData.name : 'Develpoer'}!
                 <img className='w-8 aspect-square' src={assets.hand_wave} alt='hand wave' />
             </h1>
 
